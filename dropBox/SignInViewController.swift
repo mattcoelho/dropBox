c32d280cf4ee2ea6ff96779c98f3fc30503f31f0
Matt <teus@Teus-Macbook.local>
2015-10-09 22:15:46 -0700

    performSegueWithIdentifier issue

---
+++ dropBox/SignInViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SignInViewController.swift
+//  dropBox
+//
+//  Created by Teus on 10/9/15.
+//  Copyright © 2015 Teus. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        performSegueWithIdentifier("signInSeque", sender: self)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
